cdeea4376e1fdfd20e801a7f578335d62baa8ac5
Sivasankaran Kaleeswaran <109495425+SivasankaranKaleeswaran@users.noreply.github.com>
2025-01-19 18:31:24 -0500

    Update GlobalExceptionHandler.java

--- product/src/main/java/com/ecom/microservice/product/handler/GlobalExceptionHandler.java
+++ product/src/main/java/com/ecom/microservice/product/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// used to handle exception globally
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
